0a04025438a481676f273856353a8bd7d8f6eb12
Edi Modrić <edi.modric@gmail.com>
2013-05-13 14:33:40 +0200

    Update extension name in ezinfo.php

--- ezinfo.php
+++ ezinfo.php
@@ -5,7 +5,7 @@ class ngresposiveimagesInfo
     public static function info()
     {
         return array(
-            'Name' => '<a href="http://projects.ez.no/ngresponsiveimages">ngresponsiveimages</a> extension',
+            'Name' => '<a href="http://projects.ez.no/ngresponsiveimages">Netgen Responsive Images</a> extension',
             'Version' => '1.0',
             'Copyright' => 'Copyright (&copy;) 2010-2013 <a href="http://www.netgenlabs.com" target="_blank">Netgen</a>',
             'License' => 'GNU General Public License v2.0'
