69228497f2975005a942cefe2536df06dda9039e
Edi Modrić <edi.modric@gmail.com>
2013-05-13 14:24:19 +0200

    Comment out default image.ini aliases

--- settings/image.ini.append.php
+++ settings/image.ini.append.php
@@ -1,30 +1,28 @@
-<?php /*
+<?php /* #?ini charset="utf-8"?
 
-# Image aliases used in ngresponsiveimages
+# [AliasSettings]
+# AliasList[]=default
+# AliasList[]=desktop
+# AliasList[]=mobile
+# AliasList[]=tablet
 
-[AliasSettings]
-AliasList[]=default
-AliasList[]=desktop
-AliasList[]=mobile
-AliasList[]=tablet
+# [default]
+# Reference=original
+# Filters[]=geometry/scalewidthdownonly=768
+# Filters[]=geometry/crop=768;511;0;0
 
-[default]
-Reference=original
-Filters[]=geometry/scalewidthdownonly=768
-Filters[]=geometry/crop=768;511;0;0
+# [desktop]
+# Reference=original
+# Filters[]=geometry/scalewidthdownonly=900
+# Filters[]=geometry/crop=900;600;0;0
 
-[desktop]
-Reference=original
-Filters[]=geometry/scalewidthdownonly=900
-Filters[]=geometry/crop=900;600;0;0
+# [mobile]
+# Reference=original
+# Filters[]=geometry/scalewidthdownonly=480
+# Filters[]=geometry/crop=480;320;0;0
 
-[mobile]
-Reference=original
-Filters[]=geometry/scalewidthdownonly=480
-Filters[]=geometry/crop=480;320;0;0
-
-[tablet]
-Reference=original
-Filters[]=geometry/scalewidthdownonly=768
-Filters[]=geometry/crop=768;511;0;0
+# [tablet]
+# Reference=original
+# Filters[]=geometry/scalewidthdownonly=768
+# Filters[]=geometry/crop=768;511;0;0
 */ ?>
